e7ff44799dcf58ac4a8eb6449208c8a736745e34
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-04-24 11:35:12 +0300

    ==

--- js/lesson11.js
+++ js/lesson11.js
@@ -25,7 +25,7 @@
 
 // console.log("objA", objA);
 
-///======================
+///======================-============================================
 // Алгоритм поиска свойства в цепочке прототипов:
 // 1. Поиск начинается в собственных свойствах
 // 2. Если свойства нет в сообственных, поиск переходит к цепочке прототипов
@@ -37,3 +37,73 @@
 
 // console.log("objA", objA);
 // console.log(objA.x);
+
+///==============================================
+// основа ОПП класс, экземпляр, объект, интерфейс
+
+// функции-конструкторы
+// Именование
+// Оператор new
+// - Свойство unction.prototype
+// задача
+
+// const Car = function ({ brand, model, price } = {}) {
+//   // const { brand, model, price } = config;
+//   // 2. ФУНКЦИя вызывается в контексте созданного объекта,
+//   //То есть в this записывается ссылка на него
+//   this.brand = brand;
+//   this.model = model;
+//   this.price = price;
+// Ссылка на объект возвращается в место вызова new Car
+// };
+
+// 3. В свойство thts, proto записывается ссылка на объект Car.prototype
+//- тоесть Car.prototype ЭТО ПРОТОТИП будущего объекта (экземпляра)
+
+// Car.prototype.sayHi = function () {
+//   console.log("Car.prototype.sayHi -> this", this);
+//   console.log("Hello :)");
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//   this.price = newPrice;
+// };
+
+// console.log(Car.prototype);
+
+// 1. Если функция вызывается через new, создается пустой обьект
+
+// const myCar = new Car({
+//   brand: "Audi",
+//   model: "Q3",
+//   price: 35000,
+// });
+// console.log(myCar);
+
+// myCar.sayHi();
+
+// const myCar2 = new Car({ brand: "BMW", model: "X6", price: 50000 });
+// console.log(myCar2);
+
+// myCar2.sayHi();
+
+// const myCar3 = new Car({ brand: "Audi", model: "A6", price: 65000 });
+// console.log(myCar3);
+
+// myCar3.sayHi();
+
+///===========задача 2====
+// const User = function ({ email, password } = {}) {
+//   this.email = email;
+//   this.password = password;
+// };
+
+// User.prototype.changeEmail = function (newMail) {
+//   this.email = newMail;
+// };
+
+// const mango = new User({ email: "mango@mail.com", password: 1111111 });
+
+// mango.changeEmail("my-new-mail@mail.com");
+// console.log(mango);
+////////////////////////////////////////////////////
